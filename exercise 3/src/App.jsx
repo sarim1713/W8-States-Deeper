82668f05df78264463d6d9ccddc2934a2520c8e8
Sarim Bun <sarim.bun@student.cadt.edu.kh>
2026-03-15 20:35:05 +0700

    completed

--- exercise 3/src/App.jsx
+++ exercise 3/src/App.jsx
@@ -24,6 +24,27 @@ const ORDERS = [
 export default function App() {
   const [orders, setOrders] = React.useState(ORDERS);
 
+  const getTotal = () => {
+    let result = 0;
+    orders.forEach((order) => {
+      result += order.quantity * order.price;
+    });
+
+    return result;
+  };
+
+  const handeQuantityChanged = (product, newQuantity) => {
+    setOrders(
+      orders.map((order) => {
+        if (order.product === product) {
+          return { ...order, quantity: newQuantity };
+        }
+        
+        
+      })
+    );
+  };
+
   return (
     <>
       <header>
@@ -31,10 +52,18 @@ export default function App() {
       </header>
 
       <div className="order-list">
-        <OrderCard></OrderCard>
+        {orders.map((order, index) => (
+          <OrderCard
+            key={index}
+            product={order.product}
+            productPrice={order.price}
+            quantity={order.quantity}
+            onQuantityChanged={handeQuantityChanged}
+          />
+        ))}
       </div>
 
-      <CheckoutButton total="TODO"></CheckoutButton>
+      <CheckoutButton total={getTotal()}></CheckoutButton>
     </>
   );
 }
--- exercise 3/src/components/OrderCard.jsx
+++ exercise 3/src/components/OrderCard.jsx
@@ -1,17 +1,49 @@
 import React from "react";
 
-export default function OrderCard() {
+export default function OrderCard({
+  product,
+  productPrice,
+  quantity,
+  onQuantityChanged,
+}) {
+  const format = (price) => "$ " + price;
+
+  const removeQuantity = () => {
+    if (canRemoveQuantity()) {
+      onQuantityChanged(product, quantity - 1);
+    }
+  };
+  const addQuantity = () => {
+    onQuantityChanged(product, quantity + 1);
+  };
+
+  const removeQuantityButtonStyle = () => {
+    return canRemoveQuantity() ? undefined : { backgroundColor: "#bfbfbf" };
+  };
+
+  const canRemoveQuantity = () => {
+    return quantity > 0;
+  };
+
   return (
     <div className="order">
       <div>
-        <h4>TODO NAME</h4>
-        <small>TODO PRICE</small>
+        <h4>{product}</h4>
+        <small>{format(productPrice)}</small>
       </div>
 
       <div className="order-quantity">
-        <div className="order-button">-</div>
-        <h4>TODO PRICE</h4>
-        <div className="order-button">+</div>
+        <div
+          className="order-button"
+          style={removeQuantityButtonStyle()}
+          onClick={removeQuantity}
+        >
+          -
+        </div>
+        <h4>{quantity}</h4>
+        <div className="order-button" onClick={addQuantity}>
+          +
+        </div>
       </div>
     </div>
   );
